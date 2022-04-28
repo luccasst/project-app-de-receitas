6d5b42c2c342b283e6d3d855e5f07ff5f28e3198
Andre Santos <andrefelipe_santos@outlook.com>
2022-04-28 14:56:00 -0300

    Ajust na função setStorage

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -1,10 +1,5 @@
 export default function setStorage(email) {
-  const mealsToken = !JSON.parse(localStorage.getItem('mealsToken'));
-  const cocktailsToken = !JSON.parse(localStorage.getItem('cocktailsToken'));
-  const user = !JSON.parse(localStorage.getItem('user'));
-  if (mealsToken || cocktailsToken || user) {
-    localStorage.setItem('mealsToken', JSON.stringify(1));
-    localStorage.setItem('cocktailsToken', JSON.stringify(1));
-    localStorage.setItem('user', JSON.stringify({ email }));
-  }
+  localStorage.setItem('mealsToken', JSON.stringify(1));
+  localStorage.setItem('cocktailsToken', JSON.stringify(1));
+  localStorage.setItem('user', JSON.stringify({ email }));
 }
